8c25449c7fb59fccc032542bc98c325297e075a2
Paulo Maggio <paullomagio@gmail.com>
2026-02-05 10:17:42 -0300

    Order, Instant

--- src/main/java/com/project/demo/entities/User.java
+++ src/main/java/com/project/demo/entities/User.java
@@ -1,12 +1,15 @@
 package com.project.demo.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -23,6 +26,9 @@ public class User implements Serializable{
 	private String number;
 	private String password;
 	
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
+	
 	public User() {}
 
 	
@@ -37,9 +43,6 @@ public class User implements Serializable{
 	}
 
 	
-
-	
-
 	public Long getId() {
 		return id;
 	}
@@ -97,8 +100,10 @@ public class User implements Serializable{
 	public void setPassword(String password) {
 		this.password = password;
 	}
-
-
+	
+	public List<Order> getOrders() {
+		return orders;
+	}
 
 	@Override
 	public int hashCode() {
@@ -116,6 +121,4 @@ public class User implements Serializable{
 		User other = (User) obj;
 		return Objects.equals(id, other.id);
 	}
-	
-	
 }
